27632aff71c371fcf8a47db6f7d0825c5a8906d8
stone <stone@Ashley>
2013-02-15 09:33:16 -0500

    Java client server changed a little

---
+++ src/ajclient/ajclientdata.java
@@ -0,0 +1,10 @@
+public class ajclientdata {
+
+	public ajclientdata() {
+		// default constructor
+	}
+
+	// do something whenever a message is received
+	public void recv(ajclient client, ajpacket msg) {
+	}
+}
